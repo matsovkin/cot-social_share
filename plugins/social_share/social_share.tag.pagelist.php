73a16ce5214eb7c12b267e2f2cd1f5a16f3f7099
Andrey Matsovkin <macik.spb@gmail.com>
2012-10-19 04:20:21 +0400

    Adding widget to page lists

---
+++ plugins/social_share/social_share.tag.pagelist.php
@@ -0,0 +1,29 @@
+<?PHP
+/* ====================
+[BEGIN_COT_EXT]
+Hooks=page.list.loop
+Tags=page.list.tpl:{SOCIAL_SHARE}
+[END_COT_EXT]
+==================== */
+
+/**
+ * Header file for Social share plugin
+ *
+ * @package social_share
+ * @author Andrey Matsovkin
+ * @copyright Copyright (c) 2008-2012
+ * @license Distributed under BSD License.
+ */
+
+if (!defined('COT_CODE') && !defined('COT_PLUG')) { die('Wrong URL ('.array_pop(explode("\\",__FILE__)).').'); }
+
+if (defined('SOCIAL_SHARE')) {
+	$code = share_widget_code($cfg['mainurl'].'/'.$page_tags['URL'],$page_tags['SHORTTITLE']);
+	if ($socs_cfg['auto_insert'] && !(method_exists('XTemplate','hasTag') && $t->hasTag('SOCIAL_SHARE'))) {
+		$t->vars['LIST_ROW_TEXT'] .= $code;
+	} else {
+		$t->assign('SOCIAL_SHARE',$code);
+	}
+}
+
+?>
